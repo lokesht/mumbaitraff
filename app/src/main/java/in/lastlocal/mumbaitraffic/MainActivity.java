3ba3b3eafeef325c453345d533c55698cb0ee80f
Lokesh Tiwari <tiwari9773@gmail.com>
2015-06-13 21:00:22 +0530

    Tweeter added success fully

--- app/src/main/java/in/lastlocal/mumbaitraffic/MainActivity.java
+++ app/src/main/java/in/lastlocal/mumbaitraffic/MainActivity.java
@@ -1,19 +1,63 @@
 package in.lastlocal.mumbaitraffic;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+
+import io.fabric.sdk.android.Fabric;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.tweetui.LoadCallback;
+import com.twitter.sdk.android.tweetui.TweetUi;
+import com.twitter.sdk.android.tweetui.TweetUtils;
+import com.twitter.sdk.android.tweetui.TweetView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    final String CONSUMER_KEY = "iAavzdUEc98rrid9qadJMfIiJ";
+    final String CONSUMER_SECRET_KEY ="bqYrsHZJj6289VPJMTChooyGL6BUPveTClEN0JZKKFL5Qomgov";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+       // setContentView(R.layout.activity_main);
+
+        TwitterAuthConfig authConfig =
+                new TwitterAuthConfig(CONSUMER_KEY,
+                        CONSUMER_SECRET_KEY);
+        //Fabric.with(this, new Twitter(authConfig));
+
+        Fabric.with(this, new TwitterCore(authConfig), new TweetUi());
         setContentView(R.layout.activity_main);
+
     }
 
+    public void singleTweet()
+    {
+        final LinearLayout myLayout
+                = (LinearLayout) findViewById(R.id.bike_tweet);
+
+        final long tweetId = 510908133917487104L;
+        TweetUtils.loadTweet(tweetId, new LoadCallback<Tweet>() {
+            @Override
+            public void success(Tweet tweet) {
+                myLayout.addView(new TweetView(MainActivity.this, tweet));
+            }
+
+            @Override
+            public void failure(TwitterException exception) {
+                // Toast.makeText(...).show();
+            }
+        });
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -30,6 +74,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+           startActivity(new  Intent(this,TweetListActivity.class));
             return true;
         }
 
---
+++ app/src/main/java/in/lastlocal/mumbaitraffic/TweetListActivity.java
@@ -0,0 +1,45 @@
+package in.lastlocal.mumbaitraffic;
+
+/**
+ * Created by USER on 13-Jun-15.
+ */
+import android.app.ListActivity;
+import android.os.Bundle;
+
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.tweetui.LoadCallback;
+import com.twitter.sdk.android.tweetui.CompactTweetView;
+import com.twitter.sdk.android.tweetui.TweetViewFetchAdapter;
+import java.util.Arrays;
+import java.util.List;
+
+public class TweetListActivity extends ListActivity {
+
+    List<Long> tweetIds = Arrays.asList(503435417459249153L,
+            510908133917487104L,
+            473514864153870337L,
+            477788140900347904L);
+    final TweetViewFetchAdapter adapter =
+            new TweetViewFetchAdapter<CompactTweetView>(
+                    TweetListActivity.this);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tweet_list);
+        setListAdapter(adapter);
+        adapter.setTweetIds(tweetIds,
+                new LoadCallback<List<Tweet>>() {
+                    @Override
+                    public void success(List<Tweet> tweets) {
+                        // my custom actions
+                    }
+
+                    @Override
+                    public void failure(TwitterException exception) {
+                        // Toast.makeText(...).show();
+                    }
+                });
+    }
+}
