cca66632a6303f7c60b5189d7dc1d491539227fb
Lokesh Tiwari <tiwari9773@gmail.com>
2015-06-19 22:26:05 +0530

    Changes in Wdget

---
+++ app/src/main/java/in/lastlocal/widget_sos/NewAppWidget.java
@@ -0,0 +1,64 @@
+package in.lastlocal.widget_sos;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import in.lastlocal.mumbaitraffic.R;
+
+/**
+ * Implementation of App Widget functionality.
+ * App Widget Configuration implemented in {@link NewAppWidgetConfigureActivity NewAppWidgetConfigureActivity}
+ */
+public class NewAppWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+        }
+    }
+
+    @Override
+    public void onDeleted(Context context, int[] appWidgetIds) {
+        // When the user deletes the widget, delete the preference associated with it.
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            NewAppWidgetConfigureActivity.deleteTitlePref(context, appWidgetIds[i]);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        /** For configuration */
+        // CharSequence widgetText = NewAppWidgetConfigureActivity.loadTitlePref(context, appWidgetId);
+
+        /** Construct the RemoteViews object*/
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
+       // remoteViews.setTextViewText(R.id.btn_appwidget, widgetText);
+
+        Intent configIntent = new Intent(context, SendSmsActivity.class);
+        PendingIntent configPendingIntent = PendingIntent.getActivity(context, 0, configIntent, 0);
+        remoteViews.setOnClickPendingIntent(R.id.btn_appwidget, configPendingIntent);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, remoteViews);
+    }
+}
+
---
+++ app/src/main/java/in/lastlocal/widget_sos/SendSmsActivity.java
@@ -0,0 +1,85 @@
+package in.lastlocal.widget_sos;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import in.lastlocal.constant.AppConstant;
+import in.lastlocal.mumbaitraffic.R;
+
+public class SendSmsActivity extends Activity {
+
+    String message;
+    String name;
+    List<String> contact = new ArrayList<String>();
+
+    /** */
+    SharedPreferences sharedpreferences;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        /** */
+        sharedpreferences = getSharedPreferences(AppConstant.CONTACT_PREFERENCE, Context.MODE_PRIVATE);
+        onSendMessage(null);
+    }
+
+    public void onSendMessage(View v) {
+
+        String con1, conName1, con2, conName2, con3, conName3;
+        String[] t1;
+        con1 = sharedpreferences.getString("2", "");
+        con2 = sharedpreferences.getString("3", "");
+        con3 = sharedpreferences.getString("4", "");
+
+        if (con1.length() == 0 && con2.length() == 0 && con3.length() == 0) {
+            Toast.makeText(this, "No contact saved", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (con1.length() > 10) {
+            t1 = con1.split("#");
+            con1 = t1[0];
+            conName1 = t1[1];
+
+            if (con1.length() > 9) {
+                send(con1, conName1 + " message ");
+            }
+        }
+
+        if (con2.length() > 10) {
+            t1 = con2.split("#");
+            con2 = t1[0];
+            conName2 = t1[1];
+            if (con2.length() > 9) {
+                send(con2, conName2 + " message ");
+            }
+        }
+
+        if (con3.length() > 10) {
+            t1 = con3.split("#");
+            con3 = t1[0];
+            conName3 = t1[1];
+            if (con3.length() > 9) {
+                send(con3, conName3 + " message ");
+            }
+        }
+    }
+
+    private void send(String contact, String msg) {
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(contact, null, msg, null, null);
+
+        Toast.makeText(this, msg + " messgae Send", Toast.LENGTH_SHORT).show();
+    }
+}
