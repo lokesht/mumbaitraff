3c9f05e592a9bd336c3e6e416cbdcc6ac28aa890
Lokesh Tiwari <tiwari9773@gmail.com>
2015-06-20 12:13:32 +0530

    Implemented near by police station and webview

---
+++ app/src/main/java/in/lastlocal/information/EmergencyContactActivity.java
@@ -0,0 +1,114 @@
+package in.lastlocal.information;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.desarrollodroide.libraryfragmenttransactionextended.FragmentTransactionExtended;
+
+import in.lastlocal.constant.AppConstant;
+import in.lastlocal.framework.OnFragmentInteractionListener;
+import in.lastlocal.information.fragment.EmergencyContactFragment;
+import in.lastlocal.information.fragment.EmergencyContactFragmentMarathi;
+import in.lastlocal.information.fragment.FAQFragment;
+import in.lastlocal.information.fragment.FAQFragmentMarathi;
+import in.lastlocal.mumbaitraffic.R;
+
+public class EmergencyContactActivity extends AppCompatActivity implements OnFragmentInteractionListener {
+
+    private int optionSelected = AppConstant.ANIMATE_X;
+    private EmergencyContactFragment mFirstFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faq);
+
+        // initialise();
+        if (savedInstanceState == null) {
+            mFirstFragment = new EmergencyContactFragment();
+            addFragment(mFirstFragment);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_emergency_contact, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void addFragment(Fragment mFirstFragment) {
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+        fragmentTransaction.add(R.id.fl_fragment_cantainer, mFirstFragment);
+        fragmentTransaction.commit();
+    }
+
+    /* Overloaded Method*/
+    public void replaceFragment(Fragment fragment, Bundle savedInstancState) {
+        replaceFragment(fragment, savedInstancState, true);
+    }
+
+    //To replace fragments
+    public void replaceFragment(Fragment fragment, Bundle savedInstancState, boolean isAdd) {
+        FragmentManager fragmentManager = getFragmentManager();
+        if (savedInstancState != null)
+            fragment.setArguments(savedInstancState);
+
+        if (savedInstancState == null) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+            if (isAdd)
+                transaction.addToBackStack("TAG");
+
+            transaction.replace(R.id.fl_fragment_cantainer, fragment);
+            transaction.commit();
+        }
+    }
+
+    @Override
+    public void onFragmentInteraction(View v) {
+        if (getFragmentManager().getBackStackEntryCount() == 0) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+            FragmentTransactionExtended fragmentTransactionExtended = new FragmentTransactionExtended(this, fragmentTransaction, mFirstFragment, new EmergencyContactFragmentMarathi(), R.id.fl_fragment_cantainer);
+            fragmentTransactionExtended.addTransition(optionSelected);
+            fragmentTransactionExtended.commit();
+        } else {
+            getFragmentManager().popBackStack();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        int count = getFragmentManager().getBackStackEntryCount();
+        if (count == 1) {
+            finish();
+        }
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/in/lastlocal/information/fragment/EmergencyContactFragmentMarathi.java
@@ -0,0 +1,197 @@
+package in.lastlocal.information.fragment;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import in.lastlocal.adapter.EmergancyContactAdapter;
+import in.lastlocal.adapter.ExampleAdapter;
+import in.lastlocal.constant.AppConstant;
+import in.lastlocal.customview.AnimatedExpandableListView;
+import in.lastlocal.framework.OnFragmentInteractionListener;
+
+
+import in.lastlocal.adapter.Holder.*;
+import in.lastlocal.mumbaitraffic.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link FAQFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class EmergencyContactFragmentMarathi extends Fragment {
+
+    private OnFragmentInteractionListener mListener;
+
+    private AnimatedExpandableListView listView;
+    private EmergancyContactAdapter adapter;
+
+    private Context context;
+    private Locale mLocale;
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @return A new instance of fragment FAQFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static EmergencyContactFragmentMarathi newInstance() {
+        EmergencyContactFragmentMarathi fragment = new EmergencyContactFragmentMarathi();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        context = getActivity();
+
+        /** before generating view changing config*/
+        mLocale = new Locale(AppConstant.LOCALE_ENGLISH);
+        Locale.setDefault(mLocale);
+
+        Configuration config = new Configuration();
+        config.locale = mLocale;
+        context.getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
+
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_faq, container, false);
+        onSetUP(v);
+
+        return v;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        context = getActivity();
+//
+//        //mLocale = new Locale(AppConstant.LOCALE_ENGLISH);
+//        //Locale.setDefault(mLocale);
+//
+//        Configuration config = new Configuration();
+//        config.locale = mLocale;
+//        context.getResources().updateConfiguration(config, context.getResources().getDisplayMetrics());
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        menu.clear();
+        inflater.inflate(R.menu.menu_faq_information, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.action_language:
+                mListener.onFragmentInteraction(null);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void onSetUP(View v) {
+        List<GroupItem> items = new ArrayList<GroupItem>();
+
+        // Populate our list with groups and it's children
+
+        String[] arrGroupItem = getResources().getStringArray(R.array.arr_contact_header);
+        String[] arrChildItem = getResources().getStringArray(R.array.arr_contact_number);
+
+        for (int i = 1; i < arrGroupItem.length; i++) {
+            GroupItem item = new GroupItem();
+
+            item.title = arrGroupItem[i];
+
+            // for(int j = 0; j < i; j++) {
+            ChildItem child = new ChildItem();
+            child.title = arrChildItem[0];
+            child.hint = "Too awesome";
+
+            item.items.add(child);
+            // }
+
+            items.add(item);
+        }
+
+        adapter = new EmergancyContactAdapter(getActivity());
+        adapter.setData(items);
+
+        listView = (AnimatedExpandableListView) v.findViewById(R.id.listView);
+        listView.setAdapter(adapter);
+
+        // In order to show animations, we need to use a custom click handler
+        // for our ExpandableListView.
+        listView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
+                // We call collapseGroupWithAnimation(int) and
+                // expandGroupWithAnimation(int) to animate group
+                // expansion/collapse.
+                if (listView.isGroupExpanded(groupPosition)) {
+                    listView.collapseGroupWithAnimation(groupPosition);
+                } else {
+                    listView.expandGroupWithAnimation(groupPosition);
+                }
+                return true;
+            }
+
+        });
+    }
+
+    public void onButtonPressed(View v) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(v);
+        }
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+}
