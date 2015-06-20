e4e42cc3d30afb136dbdf67dc0181020aab44202
Lokesh Tiwari <tiwari9773@gmail.com>
2015-06-20 12:16:23 +0530

    Implemented near by police station SMS

--- app/src/main/java/in/lastlocal/mumbaitraffic/EmergencySMS.java
+++ app/src/main/java/in/lastlocal/mumbaitraffic/EmergencySMS.java
@@ -192,61 +192,20 @@ public class EmergencySMS extends AppCompatActivity implements DialogEmergencyNa
         con3 = sharedpreferences.getString("4", "");
 
         if (con1.length() == 0 && con2.length() == 0 && con3.length() == 0) {
+
             Toast.makeText(this, "No contact saved", Toast.LENGTH_SHORT).show();
-            return;
+            intentToLoc();
+            //return;
         } else {
             Intent in = new Intent(this, SendSmsActivity.class);
             startActivity(in);
         }
     }
-//    public void onSendMessage(View v) {
-//
-//        String con1, conName1, con2, conName2, con3, conName3;
-//        String[] t1;
-//        con1 = sharedpreferences.getString("2", "");
-//        con2 = sharedpreferences.getString("3", "");
-//        con3 = sharedpreferences.getString("4", "");
-//
-//        if (con1.length() == 0 && con2.length() == 0 && con3.length() == 0) {
-//            Toast.makeText(this, "No contact saved", Toast.LENGTH_SHORT).show();
-//            return;
-//        }
-//
-//        if (con1.length() > 10) {
-//            t1 = con1.split("#");
-//            con1 = t1[0];
-//            conName1 = t1[1];
-//
-//            if (con1.length() > 9) {
-//                send(con1, conName1 + " message ");
-//            }
-//        }
-//
-//        if (con2.length() > 10) {
-//            t1 = con2.split("#");
-//            con2 = t1[0];
-//            conName2 = t1[1];
-//            if (con2.length() > 9) {
-//                send(con2, conName2 + " message ");
-//            }
-//        }
-//
-//        if (con3.length() > 10) {
-//            t1 = con3.split("#");
-//            con3 = t1[0];
-//            conName3 = t1[1];
-//            if (con3.length() > 9) {
-//                send(con3, conName3 + " message ");
-//            }
-//        }
-//    }
-//
-//    private void send(String contact, String msg) {
-//        SmsManager smsManager = SmsManager.getDefault();
-//        smsManager.sendTextMessage(contact, null, msg, null, null);
-//
-//        Toast.makeText(this, msg + " messgae Send", Toast.LENGTH_SHORT).show();
-//    }
+
+    public void intentToLoc() {
+        Intent in = new Intent(this, EmergencySMS.class);
+        startActivity(in);
+    }
 
     // The dialog fragment receives a reference to this Activity through the
     // Fragment.onAttach() callback, which it uses to call the following methods
