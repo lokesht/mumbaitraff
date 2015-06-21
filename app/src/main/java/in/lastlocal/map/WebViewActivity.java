112d7752d8579c2b1de8b80dd490b907c9592167
Lokesh Tiwari <tiwari9773@gmail.com>
2015-06-22 02:45:16 +0530

    success fully implemented Loader in webview of Map

--- app/src/main/java/in/lastlocal/map/WebViewActivity.java
+++ app/src/main/java/in/lastlocal/map/WebViewActivity.java
@@ -1,6 +1,8 @@
 package in.lastlocal.map;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.webkit.GeolocationPermissions;
 import android.webkit.WebChromeClient;
@@ -11,28 +13,101 @@ import in.lastlocal.mumbaitraffic.R;
 
 public class WebViewActivity extends Activity {
 
-    WebView myWebView;
+    private boolean loadingFinished = true;
+    private boolean redirect = false;
+    private WebView webView;
+    private static  int i = 0;
 
     String mapPath = "https://maps.google.com/?ll=19.0823319,72.8671099&t=m&z=12&layer=t";
+    //String mapPath = "https://maps.google.com/";
     // String mapPath ="https://www.google.co.in/maps/@19.0823319,72.8671099,13z/data=!5m1!1e1?hl=en";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_web_view);
-        myWebView = (WebView) findViewById(R.id.mapview);
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.setWebViewClient(new WebViewClient());
 
-        myWebView.setWebChromeClient(new WebChromeClient() {
+        webView = (WebView) findViewById(R.id.mapview);
+
+        startWebView(mapPath);
+    }
+
+    /** */
+    private void startWebView(String url) {
+
+        //Create new webview Client to show progress dialog
+        //When opening a url or click on link
+
+        webView.setWebViewClient(new WebViewClient() {
+            ProgressDialog progressDialog;
+
+            //If you will not use this method url links are opeen in new brower not in webview
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (!loadingFinished) {
+                    redirect = true;
+                }
+                loadingFinished = false;
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                loadingFinished = false;
+            }
+
+            //Show loader on url load
+            public void onLoadResource(WebView view, String url) {
+                if (progressDialog == null && i==0) {
+                    // in standard case YourActivity.this
+                    progressDialog = new ProgressDialog(WebViewActivity.this);
+                    progressDialog.setMessage("Loading...");
+                    progressDialog.show();
+                }
+            }
+
+
+            public void onPageFinished(WebView view, String url) {
+                try {
+                    if(!redirect){
+                        loadingFinished = true;
+                    }
+
+                    if(loadingFinished && !redirect){
+                        progressDialog.dismiss();
+                        progressDialog = null;
+                    } else{
+                        redirect = false;
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+        });
+
+        // Javascript enabled on webview
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
 
             @Override
             public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
                 callback.invoke(origin, true, false);
             }
         });
-
-        myWebView.loadUrl(mapPath);
+        webView.loadUrl(url);
     }
 
+    // Open previous opened link from history on webview when back button pressed
+    @Override
+    // Detect when the back button is pressed
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // Let the system handle the back button
+            super.onBackPressed();
+        }
+    }
 }
--- app/src/main/java/in/lastlocal/map/WebViewNearByPolice.java
+++ app/src/main/java/in/lastlocal/map/WebViewNearByPolice.java
@@ -1,9 +1,9 @@
 package in.lastlocal.map;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 import android.webkit.GeolocationPermissions;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
@@ -13,26 +13,98 @@ import in.lastlocal.mumbaitraffic.R;
 
 public class WebViewNearByPolice extends ActionBarActivity {
 
-    WebView myWebView;
-    String mapPath = "https://maps.google.com/?q=near by police station&ll=19.0823319,72.8671099&t=m&z=12&layer=t";
+    private boolean loadingFinished = true;
+    private boolean redirect = false;
+    private WebView webView;
+    private static  int i = 0;
 
+    private String mapPath = "https://maps.google.com/?q=near by police station&ll=19.0823319,72.8671099&t=m&z=12&layer=t";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_web_view_near_by_police);
-        myWebView = (WebView) findViewById(R.id.mapview);
-        myWebView.getSettings().setJavaScriptEnabled(true);
-        myWebView.setWebViewClient(new WebViewClient());
+        setContentView(R.layout.activity_web_view);
 
-        myWebView.setWebChromeClient(new WebChromeClient() {
+        webView = (WebView) findViewById(R.id.mapview);
+        startWebView(mapPath);
+    }
+
+    /** */
+    private void startWebView(String url) {
+
+        //Create new webview Client to show progress dialog
+        //When opening a url or click on link
+
+         webView.setWebViewClient(new WebViewClient() {
+            ProgressDialog progressDialog;
+
+            //If you will not use this method url links are opeen in new brower not in webview
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (!loadingFinished) {
+                    redirect = true;
+                }
+                loadingFinished = false;
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                super.onPageStarted(view, url, favicon);
+                loadingFinished = false;
+            }
+
+            //Show loader on url load
+            public void onLoadResource(WebView view, String url) {
+                if (progressDialog == null && i==0) {
+                    // in standard case YourActivity.this
+                    progressDialog = new ProgressDialog(WebViewNearByPolice.this);
+                    progressDialog.setMessage("Loading...");
+                    progressDialog.show();
+                }
+            }
+
+
+            public void onPageFinished(WebView view, String url) {
+                try {
+                    if(!redirect){
+                        loadingFinished = true;
+                    }
+
+                    if(loadingFinished && !redirect){
+                        progressDialog.dismiss();
+                        progressDialog = null;
+                    } else{
+                        redirect = false;
+                    }
+                } catch (Exception exception) {
+                    exception.printStackTrace();
+                }
+            }
+
+        });
+
+        // Javascript enabled on webview
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient() {
 
             @Override
             public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
                 callback.invoke(origin, true, false);
             }
         });
+        webView.loadUrl(url);
+    }
 
-        myWebView.loadUrl(mapPath);
+    // Open previous opened link from history on webview when back button pressed
+    @Override
+    // Detect when the back button is pressed
+    public void onBackPressed() {
+        if(webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            // Let the system handle the back button
+            super.onBackPressed();
+        }
     }
 }
